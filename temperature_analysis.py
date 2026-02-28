08a6419210b8b0f7a7fb8935c05ff18a127442b9
DevStudent2601068 <brajitha2205@gmail.com>
2026-02-28 23:24:30 +0530

    Assignment submission Python_Numpy

--- temperature_analysis.py
+++ temperature_analysis.py
@@ -13,6 +13,7 @@ temps_fahrenheit = temps_celsius * 1.8 + 32
 # Calculate average Fahrenheit
 avg_fahrenheit = round(np.mean(temps_fahrenheit), 1)
 
+
 # Print results
 print("Celsius:", temps_celsius)
 print("Fahrenheit:", temps_fahrenheit)
